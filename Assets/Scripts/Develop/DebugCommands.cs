b70191df8472951769177dde12771a0e77794c7d
Eugen1344 <eugen1344@gmail.com>
2020-07-15 16:54:17 +0300

    +debug

---
+++ Assets/Scripts/Develop/DebugCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Develop
+{
+	public class DebugCommands : MonoBehaviour
+	{
+		public Collider PlayerCollider;
+
+		public bool NoClip
+		{
+			get => PlayerCollider.enabled;
+			set => PlayerCollider.enabled = value;
+		}
+
+		private void Awake()
+		{
+			InitCommandBar();
+		}
+
+		private void InitCommandBar()
+		{
+			Commands.AddCheckbox("NoClip", () => NoClip, val => NoClip = val);
+		}
+	}
+}
